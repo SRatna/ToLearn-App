ff94fb619f544eb37e547a5d50673e198d36d55e
Sushanta <adhikariratna52@gmail.com>
2018-02-21 16:13:24 +0545

    add saga and reducer middleware to store being provided to index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+import createSagaMiddleware from 'redux-saga'
+import saga from './saga';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+sagaMiddleware.run(saga);
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
