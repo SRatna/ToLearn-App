75f58008fee4b3f61a2a29bb92e1c7903e19ca83
sratna <adhikariratna52@gmail.com>
2018-03-07 14:32:24 +0545

    Clear the saga to getTopToLearns for now.

--- src/saga/toLearn.js
+++ src/saga/toLearn.js
@@ -1,8 +1,8 @@
 /**
  * Created by sushanta on 2/23/18.
  */
-import { call } from 'redux-saga/effects';
-import { saveToLearnTextApi, getTopToLearnsApi } from '../api';
+import { call, put } from 'redux-saga/effects';
+import { saveToLearnTextApi } from '../api';
 
 export function* saveToLearnText({ text }) {
   try {
@@ -11,16 +11,8 @@ export function* saveToLearnText({ text }) {
     console.log(err);
   }
 }
-
 export function* getTopToLearns() {
   try {
-    const topToLearnsSnapshot = yield call(getTopToLearnsApi);
-    topToLearnsSnapshot.once('value', toLearns => {
-      toLearns.forEach(toLearn => {
-        console.log(toLearn.key);
-        console.log(toLearn.val());
-      });
-    });
   } catch (err) {
     console.log(err);
   }
