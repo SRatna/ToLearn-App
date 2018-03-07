ebfc775f95f6a53d8faf347c74edbf68e4683c74
sratna <adhikariratna52@gmail.com>
2018-03-07 15:17:00 +0545

    Add new styled component for styling add new to learn items.

---
+++ src/containers/Home/components/AddToLearnBar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by sushanta on 3/7/18.
+ */
+import styled from 'styled-components';
+const AddToLearnBar = styled.div`
+  display: flex;
+  padding: 10px;
+  input {
+    margin: 0 5px 0 0;
+  }
+  button {
+    margin-top: 0;
+    flex: 1;
+  }
+`;
+export default AddToLearnBar;
