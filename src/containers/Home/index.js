befe0e40ff7bcb81171a5bae2db6e735958cccef
sratna <adhikariratna52@gmail.com>
2018-03-08 11:08:04 +0545

    Add loadingSvg n voteSvg and implement loading behaviour if necessary

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -8,11 +8,13 @@ import { connect } from 'react-redux';
 import { getToLearnsRefApi } from '../../api';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import LoadingSvg from '../../components/LoadingSvg';
 import AddToLearnBar from './components/AddToLearnBar';
 import ToLearnItem from './components/ToLearnItem';
 import PersonSvg from './components/PersonSvg';
 import BookSvg from './components/BookSvg';
 import ClockSvg from './components/ClockSvg';
+import VoteSvg from './components/VoteSvg';
 import moment from 'moment';
 
 class Home extends Component {
@@ -37,6 +39,7 @@ class Home extends Component {
     })
   };
   render() {
+    console.log(this.props.toLearnItems);
     return (
       <div>
         <NavBar/>
@@ -56,16 +59,28 @@ class Home extends Component {
             Add
           </Button>
         </AddToLearnBar>
+        {this.props.toLearnItems.length === 0
+        && <LoadingSvg style={{ display: 'block', margin: 'auto', height: '35px'}}/>}
         <div>
           {
             this.props.toLearnItems.map(item => (
               <ToLearnItem key={item.key}>
-                <BookSvg/>
-                <span>{item.text}</span><br/>
-                <PersonSvg/>
-                <span>{item.email.split('@')[0]}</span><br/>
-                <ClockSvg/>
-                <span>{moment(item.createdAt).fromNow()}</span>
+                <div>
+                  <BookSvg/>
+                  <span>{item.text}</span>
+                </div>
+                <div>
+                  <PersonSvg/>
+                  <span>{item.email.split('@')[0]}</span>
+                </div>
+                <div>
+                  <ClockSvg/>
+                  <span>{moment(item.createdAt).fromNow()}</span>
+                </div>
+                <div>
+                  <VoteSvg/>
+                  <span>{item.voteCount} {item.voteCount > 1 ? ' votes' : ' vote'}</span>
+                </div>
               </ToLearnItem>
             ))
           }
