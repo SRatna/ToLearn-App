b335700829f5e581ec3dd5c64fa3ff63576cfa13
sratna <adhikariratna52@gmail.com>
2018-03-08 11:05:18 +0545

    Add styles for toLearnItem child element: div & span

--- src/containers/Home/components/ToLearnItem.js
+++ src/containers/Home/components/ToLearnItem.js
@@ -6,5 +6,18 @@ const ToLearnItem = styled.div`
   margin: 10px 10px 0;
   padding: 10px;
   box-shadow: 1px 4px 8px 1px rgba(219, 112, 147, 0.32);
+  div {
+    display: flex;
+    span {
+      flex: 1;
+      text-align: justify;
+      margin: 0 0 7px 5px;
+      font-size: 0.86em;
+      text-transform: capitalize;
+    }
+    svg {
+      margin-top: 1px;
+    }
+  }
 `;
 export default ToLearnItem;
