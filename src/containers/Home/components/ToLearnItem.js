a1f168501f7febbed312ebaf5df0953bcadddd81
sratna <adhikariratna52@gmail.com>
2018-03-08 10:10:41 +0545

    Create a new styled component for each to learn item

---
+++ src/containers/Home/components/ToLearnItem.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sushanta on 3/7/18.
+ */
+import styled from 'styled-components';
+const ToLearnItem = styled.div`
+  margin: 10px 10px 0;
+  padding: 10px;
+  box-shadow: 1px 4px 8px 1px rgba(219, 112, 147, 0.32);
+`;
+export default ToLearnItem;
