8d676a5afcb992aa31a5b973f2dad07f176d4dd0
sratna <adhikariratna52@gmail.com>
2018-03-09 15:18:48 +0545

    Write saga to handle voting of to learn item

--- src/saga/toLearn.js
+++ src/saga/toLearn.js
@@ -2,11 +2,19 @@
  * Created by sushanta on 2/23/18.
  */
 import { call } from 'redux-saga/effects';
-import { saveToLearnTextApi } from '../api';
+import { saveToLearnTextApi, voteToLearnApi } from '../api';
 
 export function* saveToLearnText({ text }) {
   try {
     yield call(saveToLearnTextApi, text);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* voteToLearn({ key }) {
+  try {
+    yield call(voteToLearnApi, key);
   } catch (err) {
     console.log(err);
   }
