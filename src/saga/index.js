a6d7085ef79ed675e985d466c3629dc7a312a4b7
sratna <adhikariratna52@gmail.com>
2018-03-09 15:13:57 +0545

    Add new watch saga for voting to learn

--- src/saga/index.js
+++ src/saga/index.js
@@ -3,13 +3,14 @@
  */
 import { takeLatest, all } from 'redux-saga/effects';
 import { loginUser, registerUser, logOutUser } from './user';
-import { saveToLearnText } from './toLearn';
+import { saveToLearnText, voteToLearn } from './toLearn';
 
 export default function* watcher() {
   yield all([
     takeLatest('LOGIN_USER', loginUser),
     takeLatest('REGISTER_USER', registerUser),
     takeLatest('LOGOUT_USER', logOutUser),
-    takeLatest('SAVE_TO_LEARN_TEXT', saveToLearnText)
+    takeLatest('SAVE_TO_LEARN_TEXT', saveToLearnText),
+    takeLatest('VOTE_TO_LEARN', voteToLearn)
   ]);
 }
