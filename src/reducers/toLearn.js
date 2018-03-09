bd73631147afc15f0544c81e4ef37d92e02b60ea
sratna <adhikariratna52@gmail.com>
2018-03-09 15:12:13 +0545

    Enable updating the votes

--- src/reducers/toLearn.js
+++ src/reducers/toLearn.js
@@ -8,6 +8,9 @@ const toLearn = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_TO_LEARN':
       return {...state, items: [action.item, ...state.items]};
+    case 'UPDATE_TO_LEARN':
+      let items = state.items.map(item => item.key === action.item.key ? action.item : item);
+      return {...state, items};
     case 'ADD_TO_LEARNS':
       return {...state, items: action.items};
     default:
