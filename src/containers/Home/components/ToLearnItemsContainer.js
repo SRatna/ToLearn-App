599236fd60a9520236bcebc96399a3bbe4786668
sratna <adhikariratna52@gmail.com>
2018-03-12 12:05:08 +0545

    Add a new to learn items container component

---
+++ src/containers/Home/components/ToLearnItemsContainer.js
@@ -0,0 +1,8 @@
+/**
+ * Created by sushanta on 3/12/18.
+ */
+import styled from 'styled-components';
+const ToLearnItemsContainer = styled.div`
+  padding: 70px 0 60px;
+`;
+export default ToLearnItemsContainer;
